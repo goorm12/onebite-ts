dee06349c5b10e639a517e47cc1be3fdf5e436f8
binwoonki <vinwoonki98@gmail.com>
2024-10-15 16:06:55 +0900

    [number] : number 타입 숫자

---
+++ section2/chapter1.ts
@@ -0,0 +1,9 @@
+// number
+// : number (타입 주석, type annotation)
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
