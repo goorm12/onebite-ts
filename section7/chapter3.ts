33cd70716c524f5944562ea69d5cacbd4d03a117
binwoonki <vinwoonki98@gmail.com>
2024-10-16 22:22:09 +0900

    [제네릭 인터페이스 & 제네릭 타입 별칭]

---
+++ section7/chapter3.ts
@@ -0,0 +1,100 @@
+/**
+ * 제네릭 인터페이스
+ */
+
+interface KeyPair<K, V> {
+  key: K;
+  value: V;
+}
+
+let keyPair: KeyPair<string, number> = {
+  key: "Key",
+  value: 0,
+};
+
+let keyPair2: KeyPair<boolean, string[]> = {
+  key: true,
+  value: ["1"],
+};
+
+/**
+ * 인덱스 시그니쳐
+ */
+
+interface NumberMap {
+  [key: string]: number;
+}
+
+let numberMap: NumberMap = {
+  key: -1231,
+  key2: 123123,
+};
+
+interface Map<V> {
+  [key: string]: V;
+}
+
+let stringMap: Map<string> = {
+  key: "value",
+};
+
+let booleanMap: Map<boolean> = {
+  key: true,
+};
+
+/**
+ * 제네릭 타입 별칭
+ */
+
+type Map2<V> = {
+  [key: string]: V;
+};
+
+let stringMap2: Map2<string> = {
+  key: "hello",
+};
+
+/**
+ * 제네릭 인터페이스의 활용 예시
+ * -> 유저 관리 프로그램
+ * -> 유저 구분 : 학생 유저 / 개발자 유저
+ */
+
+interface Student {
+  type: "student";
+  school: string;
+}
+
+interface Developer {
+  type: "developer";
+  skill: string;
+}
+
+interface User<T> {
+  name: string;
+  profile: T;
+}
+
+function goToSchool(user: User<Student>) {
+  const school = user.profile.school;
+  console.log(`${school}로 등교 완료`);
+}
+
+const developerUser: User<Developer> = {
+  name: "goorm",
+  profile: {
+    type: "developer",
+    skill: "TypeScript",
+  },
+};
+
+const studentUser: User<Student> = {
+  name: "vin",
+  profile: {
+    type: "student",
+    school: "Goorm School",
+  },
+};
+
+// goToSchool(developerUser); // 'User<Developer>' 형식의 인수는 'User<Student>' 형식의 매개 변수에 할당될 수 없습니다. 'school' 속성이 'Developer' 형식에 없지만 'Student' 형식에서 필수입니다.
+goToSchool(studentUser);
