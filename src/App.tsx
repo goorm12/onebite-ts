d7e21abf6cadfece18a2ef3ab5303beda4db74bd
binwoonki <vinwoonki98@gmail.com>
2024-10-18 17:03:41 +0900

    [App] - 삭제기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -21,13 +21,17 @@ function App() {
       },
     ]);
   };
+
+  const onClickDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
     <div>
       <h1>Todo</h1>
       <Editor onClickAdd={onClickAdd} />
       <div>
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
         ))}
       </div>
     </div>
